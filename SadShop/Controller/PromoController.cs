d4d89aaf97c6d54bac5a1f3440223214e309d45e
Raihante <raehancyber@gmail.com>
2021-01-12 00:11:15 +0700

    Add files via upload

---
+++ SadShop/Controller/PromoController.cs
@@ -0,0 +1,27 @@
+﻿using SadShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SadShop.Controller
+{
+    public class PromoController
+    {
+        public List<Diskon> diskon;
+
+        public PromoController()
+        {
+            diskon = new List<Diskon>();
+        }
+
+        public void addPromo(Diskon diskon)
+        {
+            this.diskon.Add(diskon);
+        }
+
+        public List<Diskon> getDiskon()
+        {
+            return this.diskon;
+        }
+    }
+}
---
+++ SadShop/Promo.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SadShop.Controller;
+using SadShop.Model;
+
+namespace SadShop
+{
+    /// <summary>
+    /// Interaction logic for Promo.xaml
+    /// </summary>
+    public partial class Promo : Window
+    {
+        PromoController promoController;
+        OnPromoChangedListener promoListener;
+        public Promo()
+        {
+            InitializeComponent();
+            promoController = new PromoController();
+            listBoxDaftarPromo.ItemsSource = promoController.getDiskon();
+
+            generateContentPromo();
+        }
+
+        public void SetOnPromoSelectedListener(OnPromoChangedListener promoListener)
+        {
+            this.promoListener = promoListener;
+        }
+
+        private void generateContentPromo()
+        {
+            Diskon diskon1 = new Diskon("Promo Awal tahun\nDiskon 25 % ", 25000);
+            Diskon diskon2 = new Diskon("Promo Tebus Murah\nDiskon 30 % atau maksimal 30.000", 30000);
+            Diskon diskon3 = new Diskon("Promo Natal\nPotongan 10000", 10000);
+
+            promoController.addPromo(diskon1);
+            promoController.addPromo(diskon2);
+            promoController.addPromo(diskon3);
+
+            listBoxDaftarPromo.Items.Refresh();
+        }
+
+        public void onlistBoxDaftarPromoClicked(object sender, MouseButtonEventArgs e)
+        {
+            ListBox listbox = sender as ListBox;
+            Diskon diskon = listbox.SelectedItem as Diskon;
+            this.promoListener.OnPromoSelected(diskon);
+        }
+    }
+    public interface OnPromoChangedListener
+    {
+        void OnPromoSelected(Diskon diskon);
+    }
+}
